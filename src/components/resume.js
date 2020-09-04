5f0af3a2869aa990241b7a0d02d032fdae5cb427
Daniel Slobodscoy <danielslobodscoy@gmail.com>
2020-09-04 12:20:43 -0400

    Update resume.js

--- src/components/resume.js
+++ src/components/resume.js
@@ -34,7 +34,7 @@ class Resume extends Component {
             <h5>Email</h5>
             <p>danielslobodscoy@gmail.com</p>
             <h5>Web</h5>
-            <p>https://polar-beach-97093.herokuapp.com/</p>
+            <p>https://myprodanielslobodscoy.herokuapp.com</p>
             <hr style={{borderTop: '3px solid #833fb2', width: '50%'}}/>
           </Cell>
           <Cell className="resume-right-col" col={8}>
